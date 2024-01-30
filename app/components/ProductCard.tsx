1ae88bf5f96770c4b9af1ea5268da8742a7b3ef2
LeAnh <sinhla213@gmail.com>
2024-01-30 18:04:16 +0700

    end of codewithmosh

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const ProductCard = () => {
   return (
     <div>
-      <button
+      <button className="btn btn-primary"
         onClick={() => {
           console.log("click");
         }}
